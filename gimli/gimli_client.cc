090dee17842b74af5f2e7785ec530cdbda45bdc9
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2025-11-21 16:48:45 +0200

    Use flag for port and logging library.

--- gimli/gimli_client.cc
+++ gimli/gimli_client.cc
@@ -1,10 +1,18 @@
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 #include "gimli/gimli.grpc.pb.h"
 #include "gimli/gimli.pb.h"
 #include "grpcpp/grpcpp.h"
 
-int main() {
+ABSL_FLAG(int32_t, port, 8080, "The port where to listen");
+
+int main(int argc, char **argv) {
+  absl::ParseCommandLine(argc, argv);
+  const int32_t port = absl::GetFlag(FLAGS_port);
+  const std::string address = absl::StrCat("127.0.0.1:", port);
+
   auto channel =
-      grpc::CreateChannel("127.0.0.1:8080", grpc::InsecureChannelCredentials());
+      grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
   auto stub = gimli::Gimli::NewStub(channel);
 
   grpc::ClientContext context;
--- gimli/gimli_server.cc
+++ gimli/gimli_server.cc
@@ -1,6 +1,14 @@
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/log/globals.h"
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
+#include "absl/strings/str_cat.h"
 #include "gimli/gimli.grpc.pb.h"
 #include "gimli/gimli.pb.h"
 #include "grpcpp/grpcpp.h"
+
+ABSL_FLAG(int32_t, port, 8080, "The port where to listen");
 
 namespace gimli {
 namespace {
@@ -10,9 +18,9 @@ public:
   grpc::Status Notify(grpc::ServerContext *context,
                       const NotifyRequest *request,
                       NotifyResponse *response) final {
-    std::cout << "RECEIVED " << request->input();
+    LOG(INFO) << "RECEIVED " << request->input();
 
-    response->set_output("FOOBAR " + request->input());
+    response->set_output("PAPAGEI " + request->input());
     return grpc::Status::OK;
   }
 };
@@ -20,15 +28,22 @@ public:
 } // namespace
 } // namespace gimli
 
-int main() {
+int main(int argc, char **argv) {
+  absl::InitializeLog();
+  absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
+  absl::ParseCommandLine(argc, argv);
+  const int32_t port = absl::GetFlag(FLAGS_port);
+  const std::string address = absl::StrCat("127.0.0.1:", port);
+
   gimli::GimliServiceImpl service;
 
   grpc::ServerBuilder builder;
-  builder.AddListeningPort("127.0.0.1:8080", grpc::InsecureServerCredentials());
+  builder.AddListeningPort(address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
-  std::cout << "Server started\n";
+  LOG(INFO) << "Server started on " << address;
   auto server = builder.BuildAndStart();
   server->Wait();
+  LOG(INFO) << "Server down on " << address;
 
   return 0;
 }
